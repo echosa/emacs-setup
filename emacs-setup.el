a901ccf001a0a19c1a6ae7c9804634fab27ad4ac
Jonas Bernoulli <jonas@bernoul.li>
2019-10-13 14:24:25 +0200

    Put the actual license into the LICENSE file
    Previously this file contained the permission statement instead,
    which is not the same thing as the license.
    
    Move the permission statement to the beginning of the main library,
    because that is where that is usually placed.

--- emacs-setup.el
+++ emacs-setup.el
@@ -1,5 +1,18 @@
 ;;; emacs-setup.el --- Package for maintaining your emacs configuration. Clean up your .emacs!
 
+;; Emacs-setup is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; Emacs-setup is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; emacs-setup is an emacs package that is meant to help make maintaining your
